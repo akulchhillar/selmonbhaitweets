b3ede5b94236d1fede830d197c3b4ab9fb9c7b2e
Akul Chhillar <akul.chhillar@gartner.com>
2025-03-14 01:22:45 +0530

    first commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://labywyyrykkgrnoskefd.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhYnl3eXlyeWtrZ3Jub3NrZWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MDgwNzAsImV4cCI6MjA1MTI4NDA3MH0.kR5ZwbueWfr8UjG7TiL0KzZlmsTpCv_hY_ytyl2Z1NA'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhYnl3eXlyeWtrZ3Jub3NrZWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3MDgwNzAsImV4cCI6MjA1MTI4NDA3MH0.kR5ZwbueWfr8UjG7TiL0KzZlmsTpCv_hY_ytyl2Z1NA'
 const  supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
